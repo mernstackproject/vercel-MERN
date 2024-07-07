629b2c3f2c8f72388221422c48584764c1103e83
mernstackproject <rammohangupta1769@gmail.com>
2024-07-07 08:44:56 +0530

    changes

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+app.listen(port, () => {
+  console.log("listening on port" + port);
+});
+const data = [
+  {
+    name: "hello world",
+    lname: "gupta",
+  },
+  {
+    name: "harry",
+    lname: "gupta",
+  },
+];
+app.get("/json", (req, res) => {
+  res.send(data);
+});
